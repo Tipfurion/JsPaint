86d1592bd8ea7a63af4041abdf441b178cf1b2ed
Ruslan <Ovezov3210@gmail.com>
2018-08-13 14:57:16 +0300

    rectangle release

--- paint.js
+++ paint.js
@@ -3,13 +3,18 @@ const context = canvas.getContext("2d");
 const palette=document.getElementById("palette");
 const tech=document.getElementById("techLayer");
 const techLayer=tech.getContext("2d");
+const straight= document.getElementById("straight");
+const rectangle = document.getElementById("rectangle");
+
+var rectangleDraw;
 var straightDraw;
-var isStraight=true;
+var isRectangle=false;
+var isStraight=false;
 var currentPoint={x:0, y:0};
 var previousPoint={x:0, y:0};
 var mousePressed=false;
 var color="#000000";
-const cstColor=document.getElementById("customColor");
+//const cstColor=document.getElementById("customColor");
 const custom=document.getElementById("customColor");
 custom.onchange=function(){color=this.value;cstColor.style.backgroundColor=color;}
 var brushSize=2;
@@ -18,13 +23,31 @@ Clear.onclick=function(){clear(context), clear(techLayer)};
 const brush=document.getElementById("size");
 brush.onchange=function(){
 brushSize=this.options[this.selectedIndex].value;
-
 context.lineWidth = brushSize;
 context.lineCap="round";
 techLayer.lineWidth = brushSize;
 techLayer.lineCap="round";
-
 }
+
+straight.onclick=function(){
+    if(isStraight==false){
+    straight.style.backgroundColor= "#919191";
+         isStraight=true;}
+    
+    else {
+        straight.style.backgroundColor="#eee";
+        isStraight=false;}
+    }
+rectangle.onclick=function(){
+    if(isRectangle==false){
+        rectangle.style.backgroundColor= "#919191";
+             isRectangle=true;}
+        
+        else {
+            rectangle.style.backgroundColor="#eee";
+            isRectangle=false;}
+        }
+
 for(var r=0, max=2; r<=max; r++){
     for(var g=0; g<=max; g++){
         for(var b=0; b<=max; b++){
@@ -43,7 +66,7 @@ function setColor(obj){
     tech.onmousemove= function(evt) { 
         Position(evt.offsetX, evt.offsetY);
         
-        if(mousePressed){
+        if(mousePressed==true){
             draw()
         }
         
@@ -58,7 +81,15 @@ function setColor(obj){
             context.lineTo(currentPoint.x, currentPoint.y); 
         }
         }
-        else{
+        if (isRectangle==true){
+            if(mousePressed==true){
+            context.beginPath();
+            context.strokeStyle = color;
+            context.moveTo(previousPoint.x, previousPoint.y);
+            } 
+        
+        }
+        if(isRectangle==false && isStraight==false){
         if(mousePressed==true){
         context.beginPath();
         context.strokeStyle = color;
@@ -76,33 +107,58 @@ function setColor(obj){
     }
     tech.onmousedown=function(evt){
         Position(evt.offsetX,evt.offsetY)
-        if(isStraight==true){
+        if(isStraight){
             straightDraw= setInterval(function(){
-                techLayer.clearRect(0,0,canvas.width, canvas.height);
-                techLayer.beginPath();
-                techLayer.strokeStyle = color;
-                techLayer.moveTo(previousPoint.x, previousPoint.y);
-                techLayer.lineTo(currentPoint.x, currentPoint.y);
-                techLayer.stroke();  },10)
+            techLayer.clearRect(0,0,canvas.width, canvas.height);
+            techLayer.beginPath();
+            techLayer.strokeStyle = color;
+            techLayer.moveTo(previousPoint.x, previousPoint.y);
+            techLayer.lineTo(currentPoint.x, currentPoint.y);
+            techLayer.stroke();
+          },10);
             previousPoint={x:currentPoint.x, y:currentPoint.y};
-           
+        }
+        if(isRectangle){
+            previousPoint={x:currentPoint.x, y:currentPoint.y};
+            rectangleDraw=setInterval(function(){
+            techLayer.clearRect(0,0,canvas.width, canvas.height);
+            techLayer.beginPath();
+            techLayer.strokeStyle = color;
+            techLayer.strokeRect(previousPoint.x, previousPoint.y, currentPoint.x-previousPoint.x,currentPoint.y-previousPoint.y);
+            },10)
+            
+            
         }
         draw()
         mousePressed = true;
     }
     tech.onmouseup=function(){
         mousePressed = false;
-       if(isStraight==true){
+       if(isStraight){
         techLayer.closePath();
         clearInterval(straightDraw);
         context.stroke();
         context.closePath();
         clear(techLayer);
-        
+       }
+       if(isRectangle){
+        techLayer.closePath();
+        clearInterval(rectangleDraw);
+        context.strokeRect(previousPoint.x, previousPoint.y, currentPoint.x-previousPoint.x,currentPoint.y-previousPoint.y);
+        context.closePath();
+        clear(techLayer);
        }
      
     }
-   tech.onmouseout=function(){mousePressed=false;}
+   tech.onmouseout=function(){
+    mousePressed=false;
+    if(isStraight){
+    context.stroke();
+    context.closePath();
+    clearInterval(straightDraw);
+    }
+   clearInterval(rectangleDraw);
+}
         
     
 
